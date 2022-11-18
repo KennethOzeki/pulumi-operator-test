5d3aa8b363b09ad84cf8aed6e73e905669d0d49d
KennethOzeki <k-ozeki@creationline.com>
2022-11-18 18:16:41 +0900

    update: add html file and modify main.go

--- main.go
+++ main.go
@@ -8,15 +8,44 @@ import (
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		// Create a GCP resource (Storage Bucket)
-		_, err := storage.NewBucket(ctx, "my-bucket-0927", &storage.BucketArgs{
-			Name:                     pulumi.String("my-bucket-0927"),
-			Location:                 pulumi.String("ASIA-NORTHEAST1"),
-			StorageClass:             pulumi.String("STANDARD"),
+		bucket, err := storage.NewBucket(ctx, "my-bucket-0927", &storage.BucketArgs{
+			Name:         pulumi.String("my-bucket-0927"),
+			Location:     pulumi.String("ASIA-NORTHEAST1"),
+			StorageClass: pulumi.String("STANDARD"),
+			Website: storage.BucketWebsiteArgs{
+				MainPageSuffix: pulumi.String("index.html"),
+			},
 			UniformBucketLevelAccess: pulumi.Bool(true),
 		})
 		if err != nil {
 			return err
 		}
+
+		bucketObject, err := storage.NewBucketObject(ctx, "index.html", &storage.BucketObjectArgs{
+			Bucket:      bucket.Name,
+			ContentType: pulumi.String("text/html"),
+			Source:      pulumi.NewFileAsset("index.html"),
+		})
+		bucketEndpoint := pulumi.Sprintf("http://storage.googleapis.com/%s/%s", bucket.Name, bucketObject.Name)
+		if err != nil {
+			return err
+		}
+
+		_, err = storage.NewBucketIAMBinding(ctx, "my-bucket-IAMBinding", &storage.BucketIAMBindingArgs{
+			Bucket: bucket.Name,
+			Role:   pulumi.String("roles/storage.objectViewer"),
+			Members: pulumi.StringArray{
+				pulumi.String("allUsers"),
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		// Export the DNS name of the bucket
+		ctx.Export("bucketName", bucket.Url)
+		// Export the endpointURL
+		ctx.Export("bucketEndpoint", bucketEndpoint)
 
 		return nil
 	})
